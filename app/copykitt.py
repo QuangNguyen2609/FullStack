f79e18cb7e284dd10a3a5c7a430a2bfd023528bf
QuangNguyen2609 <quangnguyentechno@gmail.com>
2024-09-28 11:17:32 +0930

    add

--- app/copykitt.py
+++ app/copykitt.py
@@ -3,12 +3,16 @@ import os
 import argparse
 import re 
 
-MAX_LENGTH = 12
+MAX_LENGTH = 32
+api_key = os.environ.get("GG_API_KEY")
 
-def generate_branding_snippet(subject, api_key):
+def strip_markdown_formatting(text):
+    return re.sub(r'\*|\*\*|\n', '', text)
+
+def generate_branding_snippet(subject):
     palm.configure(api_key=api_key)
     model_name = "models/text-bison-001"
-    prompt = f"Generate upbeat branding snippet for {subject}"
+    prompt = f"Generate upbeat branding snippet for {subject}. Keep it short and sweet under one sentence."
     
     completion = palm.generate_text(
         model=model_name,
@@ -17,13 +21,13 @@ def generate_branding_snippet(subject, api_key):
         max_output_tokens=800,
     )
     response = completion.result.strip()
-    return response
+    return strip_markdown_formatting(response)
 
 # same def but for keyword
-def generate_keyword_snippet(subject, api_key):
+def generate_keyword_snippet(subject):
     palm.configure(api_key=api_key)
     model_name = "models/text-bison-001"
-    prompt = f"Generate related branding keyword for {subject}"
+    prompt = f"Generate related branding keyword for {subject}. Generate more than 3 keywords. Don't include any numbers or special characters."
     
     completion = palm.generate_text(
         model=model_name,
@@ -32,7 +36,12 @@ def generate_keyword_snippet(subject, api_key):
         max_output_tokens=800,
     )
     response = completion.result
-    keyword_arr = [line.strip() for line in response.strip().split('\n')]
+    print(response)
+    keyword_arr = [
+        strip_markdown_formatting(line) 
+        for line in (response.strip().split('\n') if '\n' in response else response.strip().split(',')) 
+        if len(line) > 0
+    ]
     return keyword_arr
 
 def validate_length(text):
@@ -45,14 +54,13 @@ def main():
     parser.add_argument("--subject", type=str, required=True)
     args = parser.parse_args()
     subject = args.subject
-    api_key = os.environ.get("GG_API_KEY")
     if validate_length(subject) == False:
         raise ValueError(f"Subject length must be less than {MAX_LENGTH} characters")
     print(f"Generating branding snippet for {subject}:")
-    snippet = generate_branding_snippet(subject, api_key)
+    snippet = generate_branding_snippet(subject)
     print("Snippet: ",snippet)
     print(f"Generating branding keywords for {subject}:")
-    keywords_arr = generate_keyword_snippet(subject, api_key)
+    keywords_arr = generate_keyword_snippet(subject)
     print("keywords: ", keywords_arr)
 
 
